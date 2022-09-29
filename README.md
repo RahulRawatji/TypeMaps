77f4d365dac0658230eebc4f4e48bd056ecf730f
Rahul Rawat <rahul.rawat3@byjus.com>
2022-09-29 11:23:06 +0530

    feat: :art: typeMap basic

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # TypeMaps
+
+- npm install -g typescript ts-node
+# ts-node compiles the ts code and runs it in a single command
+
+- install parcel-bundler
+- npm i
+# run parcel index.html
---
+++ src/Company.ts
@@ -0,0 +1,24 @@
+import { faker } from '@faker-js/faker';
+import { Mappable } from './CustomMap';
+
+export class Company implements Mappable {
+  name: string;
+  catchPhrase: string;
+  location: {
+    lat: number;
+    lng: number;
+  }
+
+  constructor() {
+    this.name = faker.company.name();
+    this.catchPhrase = faker.company.catchPhrase();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude())
+    }
+  };
+
+  markerContent(): string {
+    return `<h4>Company Name ${this.name} </h4>`
+  }
+}
